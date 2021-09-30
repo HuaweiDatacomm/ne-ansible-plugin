5e11f6875852ec3f4b59457b47e105f29434c051
lllyfeng <frank.fengchong@huawei.com>
2021-09-30 11:27:29 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ne-ansible-plugin',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/HuaweiDatacomm/ne-ansible-plugin',
+    license='Apache License, Version 2.0',
+    author='frank feng',
+    author_email='frank.fengchong@huawei.com',
+    description='huawei net-engine plugin for ansible'
+)
