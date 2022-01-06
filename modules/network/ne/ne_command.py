67f6ca8e64ce28c32c1bad87339f182c1ced6c2a
niubiwocao <93188931+niubiwocao@users.noreply.github.com>
2022-01-06 23:31:25 +0800

    Update ne_command.py

--- modules/network/ne/ne_command.py
+++ modules/network/ne/ne_command.py
@@ -133,8 +133,8 @@ import time
 import traceback
 import sys
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.network.ne.ne import ne_argument_spec, check_args
-from ansible.module_utils.network.ne.ne import run_commands
+from ansible_collections.huaweidatacom.ne.plugins.module_utils.network.ne.ne import ne_argument_spec, check_args
+from ansible_collections.huaweidatacom.ne.plugins.module_utils.network.ne.ne import run_commands
 from ansible.module_utils.network.common.parsing import Conditional
 from ansible.module_utils.network.common.utils import ComplexList
 from ansible.module_utils.six import string_types
