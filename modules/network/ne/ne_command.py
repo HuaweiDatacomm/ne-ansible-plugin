326d29eee77beb91b431cf7359d380db1458f076
niubiwocao <93188931+niubiwocao@users.noreply.github.com>
2022-01-07 11:03:00 +0800

    Update ne_command.py

--- modules/network/ne/ne_command.py
+++ modules/network/ne/ne_command.py
@@ -135,8 +135,8 @@ import sys
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.huaweidatacom.ne.plugins.module_utils.network.ne.ne import ne_argument_spec, check_args
 from ansible_collections.huaweidatacom.ne.plugins.module_utils.network.ne.ne import run_commands
-from ansible.module_utils.network.common.parsing import Conditional
-from ansible.module_utils.network.common.utils import ComplexList
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.parsing import Conditional
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import ComplexList
 from ansible.module_utils.six import string_types
 from ansible.module_utils._text import to_native
 
